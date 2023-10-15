ea58bd442ded00261da0691dfc929211ad8a4892
abdullapro2006 <abdullahmanafli38@gmail.com>
2023-10-15 23:46:49 +0400

    pironia task

---
+++ Pustok/Pustok/Database/DomainModels/Product.cs
@@ -0,0 +1,25 @@
+﻿namespace Pustok.Database.DomainModels
+{
+    public class Product
+    {
+        public Product()
+        {
+            Id = ++DbContext._productId;
+        }
+
+        public Product(string name, decimal price, int rating)
+        {
+            Id = ++DbContext._productId;
+            Name = name;
+            Price = price;
+            Rating = rating;
+        }
+
+      
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Rating { get; set; }
+    }
+}
